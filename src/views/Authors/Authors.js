4ac4fa8862ec509cf276c71a0d94b3accfd277fc
sudoghut <sudospace@gmail.com>
2020-12-21 20:36:26 +0800

    add more authors

--- src/views/Authors/Authors.js
+++ src/views/Authors/Authors.js
@@ -63,6 +63,33 @@ class Authors extends Component {
                     <tr><td>1482</td><td>123973</td><td>申時行</td><td>1535</td><td>1614</td></tr>
                     <tr><td>3036</td><td>33860</td><td>呂柟</td><td>1479</td><td>1542</td></tr>
                     <tr><td>3083</td><td>126976</td><td>毛憲</td><td>1469</td><td>1535</td></tr>
+                    <tr><td>3133</td><td>30713</td><td>黃宗羲</td><td>1610</td><td>1695</td></tr>
+                    <tr><td>3154</td><td>34252</td><td>顧炎武</td><td>1613</td><td>1682</td></tr>
+                    <tr><td>3201</td><td>29584</td><td>陳子龍</td><td>1608</td><td>1647</td></tr>
+                    <tr><td>3239</td><td>30849</td><td>高攀龍</td><td>1562</td><td>1626</td></tr>
+                    <tr><td>3475</td><td>29707</td><td>劉永澄</td><td>1576</td><td>1612</td></tr>
+                    <tr><td>3529</td><td>30239</td><td>錢謙益</td><td>1582</td><td>1664</td></tr>
+                    <tr><td>3599</td><td>30598</td><td>徐光啟</td><td>1562</td><td>1633</td></tr>
+                    <tr><td>3601</td><td>30602</td><td>徐世溥</td><td>1608</td><td>1658</td></tr>
+                    <tr><td>3614</td><td>30657</td><td>黃淳耀</td><td>1605</td><td>1645</td></tr>
+                    <tr><td>3629</td><td>34742</td><td>顧憲成</td><td>1550</td><td>1612</td></tr>
+                    <tr><td>3728</td><td>34744</td><td>馮從吾</td><td>1556</td><td>1627</td></tr>
+                    <tr><td>3765</td><td>34747</td><td>鄒元標</td><td>1551</td><td>1624</td></tr>
+                    <tr><td>4114</td><td>34786</td><td>沈思孝</td><td>1542</td><td>1611</td></tr>
+                    <tr><td>4139</td><td>34919</td><td>屠隆</td><td>1542</td><td>1605</td></tr>
+                    <tr><td>4468</td><td>30240</td><td>馮夢禎</td><td>1548</td><td>1605</td></tr>
+                    <tr><td>5059</td><td>30734</td><td>倪元璐</td><td>1593</td><td>1644</td></tr>
+                    <tr><td>5095</td><td>34926</td><td>沈壽民</td><td>1607</td><td>1675</td></tr>
+                    <tr><td>5256</td><td>35076</td><td>魏禧</td><td>1624</td><td>1680</td></tr>
+                    <tr><td>5302</td><td>35118</td><td>吳偉業</td><td>1609</td><td>1671</td></tr>
+                    <tr><td>5309</td><td>35127</td><td>楊漣</td><td>1571</td><td>1625</td></tr>
+                    <tr><td>5447</td><td>35222</td><td>袁宏道</td><td>1568</td><td>1610</td></tr>
+                    <tr><td>5904</td><td>54845</td><td>徐燦</td><td>1610</td><td>1678</td></tr>
+                    <tr><td>5987</td><td>54903</td><td>李因</td><td>1616</td><td>1685</td></tr>
+                    <tr><td>6018</td><td>56589</td><td>侯方域</td><td>1618</td><td>1654</td></tr>
+                    <tr><td>6037</td><td>59750</td><td>程嘉燧</td><td>1565</td><td>1644</td></tr>
+                    <tr><td>6050</td><td>63882</td><td>孫承宗</td><td>1563</td><td>1638</td></tr>
+
                     </tbody>
                 </Table>
             )
