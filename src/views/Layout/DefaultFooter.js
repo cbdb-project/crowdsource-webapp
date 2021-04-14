80f8eaa9bc56fbac19bb3eb9e0215355485d9a52
sudoghut <sudospace@gmail.com>
2021-04-15 00:10:14 +0800

    update footer

--- src/views/Layout/DefaultFooter.js
+++ src/views/Layout/DefaultFooter.js
@@ -15,7 +15,7 @@ class DefaultFooter extends Component {
 
     return (
       <React.Fragment>
-        <span>Crowdsourcing System for Association Data &copy; 2021 <a href="https://cbdb.fas.harvard.edu/" target="_blank">CBDB</a> | <a href="https://github.com/cbdb-project/crowdsource-webapp" target="_blank">GitHub</a> | in Collaboration with the <a href="https://ccs.ncl.edu.tw/expertDB5.aspx" target="_blank">CCS</a></span>
+        <span>Crowdsourcing System for Association Data &copy; 2021 <a href="https://cbdb.fas.harvard.edu/" target="_blank">CBDB</a> | <a href="https://github.com/cbdb-project/crowdsource-webapp" target="_blank">GitHub</a> | in Collaboration with the <a href="https://ccs.ncl.edu.tw/expertDB5.aspx" target="_blank">Center for Chinese Studies</a></span>
         <span className="ml-auto">Content on <a href="https://csa.cbdb.fas.harvard.edu:81/" target="_blank">CSA</a> is licensed under a <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank">CC BY-NC-ND 4.0 </a> License</span>
       </React.Fragment>
     );
